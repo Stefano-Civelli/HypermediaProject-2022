649535d649eb73b908d0d1fb7835771e0fd17ef7
Stefano Civelli <stefano.civelli@gmail.com>
2022-05-28 23:08:38 +0200

    eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,7 @@ module.exports = {
   extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
   plugins: [],
   // add your custom rules here
-  rules: {},
+  rules: {
+    rules: [{"vue/no-multiple-template-root": "off" }, {'vue/multi-word-component-names': 'off',}]
+  },
 }
