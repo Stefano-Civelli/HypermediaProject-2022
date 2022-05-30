2090b80f8db687a326e2eb7039dbea3c9cdceb72
stefanoderr <stefano.derrico@mail.polimi.it>
2022-05-30 23:29:52 +0200

    ItineraryComponent and itinerary page added to gitignore (temporairly)

---
+++ pages/itineraries.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="page container">
+    <h1>Itineraries</h1>
+    <itinerary-component 
+        v-for="itinerary in itineraryList" 
+        :key="itinerary.id" 
+        :name="itinerary.name" 
+        :img="itinerary.img"/>
+  </div>
+</template>
+
+<script>
+import ItineraryComponent from '~/components/ItineraryComponent.vue'
+export default {
+  components: { ItineraryComponent },
+  async asyncData({ $axios }) {
+    const { itineraryList } = await $axios.get('/api/itinerary/list')
+    return {
+      itineraryList
+    }
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style>
+
+</style>
