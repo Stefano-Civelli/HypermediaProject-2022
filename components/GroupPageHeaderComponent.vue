48aee4dc4b12c6463654bffb85766c8d4aedc07d
Stefano Civelli <stefano.civelli@gmail.com>
2022-06-03 18:50:31 +0200

    Moved the group page header in its own component for convenience

---
+++ components/GroupPageHeaderComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="page-title d-flex justify-content-center align-items-center">
+    <h1 class="display-4 fw-bold my-border">{{ text }}</h1>
+  </div>
+</template>
+
+<style scoped>
+.page-title {
+  width: 100%;
+  height: 120px;
+  text-align: center;
+}
+</style>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
