95504b1178e61c01e366a6acd2902da99941d968
stefanoderr <stefano.derrico@mail.polimi.it>
2022-06-13 02:13:54 +0200

    KoT itinerary page rebuilt: layout copied and adapted from pois KoT; added the itinerary poi number to itin-poi-component in props as idx

--- pages/details/itinerary/_id.vue
+++ pages/details/itinerary/_id.vue
@@ -1,257 +1,198 @@
 <template>
-  <div class="page">
-    <OldGPHeader
-      :text="name"
-      textColor="rgb(248, 249, 250)"
-      bgColor="rgb(243, 134, 48)"
-    />
-    <div class="page-body">
-      <div class="itinerary-img-container">
-        <img :src="img" alt="Pinacoteca di Brera" class="itinerary-img" />
+    <div class="container">
+        <div class="p-4 p-md-5 my-4 text-white rounded top-img img shadow-lg d-flex justify-content-center align-items-center"
+            :style="{ 'background-image': 'url(' + img + ')' }">
+            <h1 class="display-4 my-title">
+                {{ name }}
+            </h1>
+        </div>
         <nav aria-label="breadcrumb">
-          <ol class="breadcrumb-ol">
-            <li class="breadcrumb-li">
-              <nuxt-link :to="`/itineraries`">
-                <div class="breadcrumb-btn">Itineraries</div>
-              </nuxt-link>
-            </li>
-            <li class="breadcrumb-lli">{{ name }}</li>
-          </ol>
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="/pois">Points of Interest</a></li>
+                <li class="breadcrumb-item">
+                    {{ name }}
+                </li>
+            </ol>
         </nav>
-      </div>
-      <hr />
-      <p class="itinerary-description" lang="it">{{ description }}</p>
-      <hr />
-      <p class="pois-introduction" lang="it">Attractions proposed:</p>
-      <div class="pois-container">
-        <itin-poi-component
-          class="poi-component"
-          v-for="poi in pois"
-          :key="poi.id"
-          :id="poi.id"
-          :address="poi.address"
-          :name="poi.name"
-          :description="
-            poi.description.length > 130
-              ? poi.description.slice(0, 70) + '...'
-              : poi.description
-          "
-          :position="poi.position"
-          :practical_info="poi.practical_info"
-          :ticket_price="poi.ticket_price"
-          :poi_imgs="poi.poi_imgs"
-          :events="poi.events"
-        />
-      </div>
-      <div class="group-links">
-        <div
-          v-if="id !== 1"
-          @click="previousItinerary()"
-          class="previous-btn previous-btn-hov"
-        >
-          Previous
+
+        <div class="row g-5 mt-3">
+            <div class="col-md-8">
+                <h3 class="pb-4 mb-4 fst-italic border-bottom">
+                    "{{ name }}" - What to know
+                </h3>
+
+                <article class="blog-post">
+                    <h2 class="blog-post-title mb-1">Description</h2>
+                    <br />
+                    <p>
+                        {{ description }}
+                    </p>
+                    <h2 class="mb-4">Related attractions</h2>
+
+                    <!-- Events circles -->
+                    <div class="row">
+                            <div class="pois-container">
+                                <itin-poi-component class="poi-component" v-for="poi in pois" 
+                                    :key="poi.id" :id="poi.id"
+                                    :address="poi.address" 
+                                    :name="poi.name" 
+                                    :description="poi.description.length > 1000 ? poi.description.slice(0, 950) + '...' : poi.description" 
+                                    :position="poi.position" :practical_info="poi.practical_info"
+                                    :ticket_price="poi.ticket_price" 
+                                    :poi_imgs="poi.poi_imgs" 
+                                    :events="poi.events"
+                                    :idx="pois.indexOf(poi) + 1" />
+                            </div>
+                        <h2 class="blog-post-title mb-1">Where is it?</h2>
+                        <br />
+                        <div class="px-4 py-2">
+                            <iframe :src="map_src" width="100%" height="400" referrerpolicy="no-referrer-when-downgrade"
+                                class="border my-map rounded-4"></iframe>
+                        </div>
+                    </div>
+                    <!-- /.row -->
+                </article>
+
+                <nav class="blog-pagination mt-5" aria-label="Pagination">
+                    <button class="btn btn-outline-secondary rounded-pill" @click="prev">
+                        Previous
+                    </button>
+                    <button class="btn btn-outline-primary rounded-pill" @click="next" href="#">
+                        Next
+                    </button>
+                </nav>
+            </div>
+
+            <div class="col-md-4">
+                <div class="position-sticky" style="top: 5rem">
+                    <div class="p-4 mb-3 bg-light rounded">
+                        <h4 class="fst-italic">Practical Info</h4>
+                        <p class="mb-0">Attractions included: {{ pois.length }}</p>
+                        <p>Duration: {{ duration }} </p>
+                    </div>
+
+                    <div class="p-4">
+                        <h4 class="fst-italic">Events on the way</h4>
+                        <ol class="list-unstyled mb-0">
+                            <li v-for="event in events" :key="event.id">
+                                <nuxt-link :to="`/details/event/${event.name}`">
+                                    {{ event.name }}
+                                </nuxt-link>
+                            </li>
+                        </ol>
+                    </div>
+
+                    <div class="p-4">
+                        <h4 class="fst-italic">Elsewhere</h4>
+                        <ol class="list-unstyled">
+                            <li><a href="#">GitHub</a></li>
+                            <li><a href="#">Twitter</a></li>
+                            <li><a href="#">Facebook</a></li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div
-          v-if="id !== maxItinId"
-          @click="nextItinerary()"
-          class="next-btn next-btn-hov"
-        >
-          Next
-        </div>
-      </div>
     </div>
-  </div>
 </template>
 
-<script>
-import ItinPoiComponent from '~/components/Itin-Poi-Component.vue'
-import OldGPHeader from '~/components/OldGPHeader.vue'
-export default {
-  components: { ItinPoiComponent, OldGPHeader },
-  name: 'ItineraryPage',
-  async asyncData({ route, $axios }) {
-    const { id } = route.params
-    const { data } = await $axios.get('/api/itinerary/' + id)
-    const data2 = await $axios.get('/api/pois-by-itin-id/' + id)
-    const data3 = await $axios.get('/api/maxItinId/')
-    return {
-      id: data.id,
-      name: data.name,
-      img: data.img,
-      description: data.description,
-      duration: data.duration,
-      pois: data2.data,
-      maxItinId: data3.data,
-    }
-  },
-  head() {
-    return {
-      title: this.name,
-    }
-  },
-  mounted() {},
-  methods: {
-    nextItinerary() {
-      if (this.id + 1 > this.maxItinId) {
-        this.$router.push(`/details/itinerary/${this.maxItinId}`)
-      } else {
-        this.$router.push(`/details/itinerary/${this.id + 1}`)
-      }
-    },
-    previousItinerary() {
-      if (this.id === 1) {
-        this.$router.push(`/details/itinerary/1`)
-      } else {
-        this.$router.push(`/details/itinerary/${this.id - 1}`)
-      }
-    },
-  },
-}
-</script>
-
 <style scoped>
-.pois-introduction {
-  font-size: 3.5ch;
+.my-map {
+    margin: auto;
 }
 
-.pois-container {
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: center;
-  align-items: center;
-  padding: clamp(5px, 5vw, 10px);
+.rounded-4 {
+    border-radius: 15px;
 }
 
-.poi-component {
-  margin: 4px;
+.my-border {
+    border: 1px solid red;
 }
 
-.page-header {
-  justify-content: center;
+.my-title {
+    text-shadow: 2px 2px 0px rgba(0, 0, 0, 0.4);
 }
 
-.itinerary-title {
-  font-size: 25px;
-  font-weight: 400;
-  padding: 7px;
-  padding-top: 8px;
-  margin: 0px;
+.my-img {
+    object-fit: cover;
+    object-position: 20% 50%;
 }
 
-.page-title {
-  background-color: rgb(0, 140, 255);
-  text-align: center;
-  width: 100vw;
-  color: rgb(255, 255, 255);
+.top-img {
+    height: 300px;
 }
 
-.page-body {
-  display: flex;
-  flex-flow: column nowrap;
-  align-items: center;
-  background-color: rgb(233, 233, 233);
+.img {
+    background-size: cover;
+    background-position: 0% 70%;
 }
 
-.itinerary-img-container {
-  display: flex;
-  flex-flow: column;
-  justify-content: center;
-  align-items: center;
-  width: 100vw;
-  background-color: rgb(255, 242, 225);
-  padding: 4px;
-  padding-bottom: 6px;
+a {
+    color: inherit;
+    text-decoration: none;
 }
 
-.itinerary-img {
-  width: 60vw;
-  min-width: 120px;
-  max-width: 600px;
-  height: 40vw;
-  min-height: 80px;
-  max-height: 400px;
-  border-radius: 16px;
-  border: 1px solid rgb(189, 189, 189);
+a {
+    background: linear-gradient(to right, rgb(33, 37, 41), rgb(33, 37, 41)),
+        linear-gradient(to right, rgb(0, 165, 91), rgb(0, 165, 91));
+    background-size: 100% 1px, 0 1px;
+    background-position: 100% 100%, 0 100%;
+    background-repeat: no-repeat;
+    transition: background-size 500ms;
 }
 
-hr {
-  margin: 0px;
-  color: rgb(85, 85, 85);
-  height: 1px;
-  width: 30%;
-}
-
-.breadcrumb-navbar {
-  display: block;
-}
-
-.breadcrumb-ol {
-  margin: 10px;
-  align-items: center;
-  display: flex;
-  flex-wrap: wrap;
-  padding: 0px;
-  list-style: none;
-}
-
-.breadcrumb-li {
-  list-style: none;
-}
-
-.breadcrumb-li::after {
-  content: url(assets\breadrumb-divider.svg);
-  margin-left: 6px;
-  margin-right: 6px;
-}
-
-.breadcrumb-btn,
-.previous-btn,
-.next-btn {
-  color: #616161;
-  background-color: #ffffffd8;
-  border: 2px solid #616161;
-  display: inline-block;
-  text-align: center;
-  text-decoration: none;
-  cursor: pointer;
-  user-select: none;
-  padding: 0.3rem 0.6rem;
-  font-size: 1rem;
-  border-radius: 1.25rem;
-
-  transition-duration: 0.3s;
-  transition-timing-function: ease;
-}
-
-.breadcrumb-btn:hover,
-.previous-btn-hov:hover {
-  background-color: #616161;
-  color: #ffffffd8;
-}
-
-.next-btn {
-  color: #0d6efd;
-  border-color: #0d6efd;
-}
-
-.next-btn-hov:hover {
-  background-color: #0d6efd;
-  color: #ffffffd8;
-}
-
-.group-links {
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: center;
-  width: 80vw;
-  padding-bottom: 0.8%;
-}
-
-.itinerary-description {
-  font-size: 20px;
-  padding: 1.5vw;
-  margin: 0px;
-  padding-left: 3vw;
-  padding-right: 3vw;
+a:hover {
+    background-size: 0 2px, 100% 2px;
 }
 </style>
+
+<script>
+export default {
+    name: 'ItineraryPage',
+    async asyncData({ route, $axios }) {
+        const { id } = route.params
+        const { data } = await $axios.get('/api/itinerary/' + id)
+        const data2 = await $axios.get('/api/pois-by-itin-id/' + id)
+        const data3 = await $axios.get('/api/maxItinId/')
+        const eventList = []
+        for(const poi of data2.data) {
+            for(const evento of poi.events) {
+                eventList.push(evento)
+            }
+        }
+        return {
+            id: data.id,
+            name: data.name,
+            img: data.img,
+            description: data.description,
+            duration: data.duration,
+            map_src: data.map_src,
+            pois: data2.data,
+            maxItinId: data3.data,
+            events: eventList
+        }
+    },
+    head() {
+        return {
+            title: this.name,
+        }
+    },
+    mounted() { },
+    methods: {
+        next() {
+            if (this.id + 1 > this.maxItinId) {
+                this.$router.push(`/details/itinerary/${this.maxItinId}`)
+            } else {
+                this.$router.push(`/details/itinerary/${this.id + 1}`)
+            }
+        },
+        prev() {
+            if (this.id === 1) {
+                this.$router.push(`/details/itinerary/1`)
+            } else {
+                this.$router.push(`/details/itinerary/${this.id - 1}`)
+            }
+        },
+    },
+}
+</script>
