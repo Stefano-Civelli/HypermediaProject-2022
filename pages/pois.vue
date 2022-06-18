08dcc5d56701cab17d9253e53bc1762a85602987
Stefano Civelli <stefano.civelli@gmail.com>
2022-06-18 12:15:48 +0200

    boia

--- pages/pois.vue
+++ pages/pois.vue
@@ -60,7 +60,7 @@ export default {
 
 <style scoped>
 .hero-section-parallax {
-  background-image: url('~/assets/life-background.jpg');
+  background-image: url('~/assets/Life-Background.jpg');
 }
 
 .page-component {
