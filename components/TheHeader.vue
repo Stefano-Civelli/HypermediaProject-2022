6c6cb675e26ba904b19eb12f14462c47e00a9617
pietrobernardelle <pietro.bernardelle@icloud.com>
2022-07-01 00:05:52 +0200

    small fix

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -3,7 +3,6 @@
     <nav
       :key="$route.path"
       class="my-navbar navbar navbar-expand-lg navbar-dark px-3 header position-fixed w-100 top-0"
-      :key="$route.path"
     >
       <a class="navbar-brand navbar-text navbar-home-button" href="/"
         >Milano City</a
