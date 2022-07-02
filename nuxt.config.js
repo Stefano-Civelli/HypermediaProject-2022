70bff7a334a6e41c96373e18f8603b471cce843d
Stefano Civelli <stefano.civelli@gmail.com>
2022-07-02 12:32:16 +0200

    heroku version

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
-  ssr: false,
+  ssr: true,
   serverMiddleware: [
     {
       path: '/api',
@@ -87,13 +87,12 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-
-    baseURL: 'http://localhost:3000',
     /*
-    for heroku
+    baseURL: 'http://localhost:3000',
+    
+    for heroku*/
 
     baseURL: 'https://hypermedia-project-2022.herokuapp.com',
-    */
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
